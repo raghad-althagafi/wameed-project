95e02c5d61cef976b17046681f0ee8fb347a69ec
Leena-alnahdi <lalnahdi0008@stu.kau.edu.sa>
2026-05-10 04:25:17 +0300

    remove comments

--- backend/FlaskMain.py
+++ backend/FlaskMain.py
@@ -3,11 +3,6 @@ from flask_cors import CORS
 from Data.predicted_fire_data import predictions_bp 
 from Data.detected_fire_data import detections_bp
 
-# ----------------- PAGES ROUTES ------------------
-# from flask import render_template
-# from routes.pages import pages_bp
-# ----------------- PAGES ROUTES ------------------
-
 from Singleton.gee_connection import GEEConnection #import GEEConnection calss from gee_connection file
 from FireSpreadEstimator import fire_spread_bluePrint #import fire_spread_bluePrint from FireSpreadEstimator file
 from FireThreatEstimator import fire_threat_bp 
@@ -18,11 +13,6 @@ from auth import auth_bp
 
 GEEConnection.get_instance() # initalize google earth engine connection once turning on the server, so whenever connection needed after that it will be returned
 
-# ----------------- PAGES ROUTES ------------------
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # backend/
-# FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "frontend"))
-# PAGES_DIR = os.path.join(FRONTEND_DIR, "pages")
-# ----------------- PAGES ROUTES ------------------
 
 app = Flask(__name__) # create the server
 
@@ -39,7 +29,6 @@ CORS(
     methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     supports_credentials=True
 )
-# GEEConnection.get_instance() # initalize google earth engine connection once turning on the server, so whenever connection needed after that it will be returned
 
 #register BluePrints
 app.register_blueprint(fire_spread_bluePrint)
